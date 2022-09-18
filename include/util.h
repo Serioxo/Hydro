35b4e171820310dee0d067478b3b6c5cb61e96d2
Jakub <jakubmigac16@gmail.com>
2022-09-18 01:40:22 +0100

    Argument handling

---
+++ include/util.h
@@ -0,0 +1,7 @@
+#ifndef HYDRO UTIL_H
+#define HYDRO_UTIL_H
+
+char* read_file(const char* path);
+
+
+#endif // HYDRO_UTIL_H
---
+++ src/utils.c
@@ -0,0 +1,6 @@
+#include "../include/util.h"
+
+char *read_file(const char *path)
+{
+
+}
